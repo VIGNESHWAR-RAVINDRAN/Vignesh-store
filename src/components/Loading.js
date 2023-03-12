0bdcbc263c3784b426205f5303a6cc3b79209697
VIGNESHWAR-RAVINDRAN <104151590+VIGNESHWAR-RAVINDRAN@users.noreply.github.com>
2023-03-12 15:20:30 +0530

    Add files via upload

---
+++ src/components/Loading.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Loading = () => {
+  return <div className='section section-center'>
+   <div className='loaading'></div> 
+  </div>
+}
+
+export default Loading
---
+++ src/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+import { ProductsProvider } from './context/products_context';
+import { FilterProvider } from './context/filter_context';
+import { CartProvider } from './context/cart_context';
+import { UserProvider } from './context/user_context';
+import { Auth0Provider } from '@auth0/auth0-react';
+//dev-lecft8h47ud22a0l.us.auth0.com(Domain)
+//bJAXpFgcwL1JJOdqapfD8gsWWh74Je62
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// root.render(<App />);
+
+ReactDOM.render(
+  <Auth0Provider
+    domain={process.env.REACT_APP_AUTH_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+      cacheLocation:'Localstorage'
+    }}>
+  <UserProvider>
+
+  
+ <ProductsProvider>
+   <FilterProvider>
+    <CartProvider>
+    <App/>
+    </CartProvider>
+   
+   </FilterProvider>
+    
+ </ProductsProvider>
+ </UserProvider>
+ </Auth0Provider>,
+   document.getElementById('root') 
+)
